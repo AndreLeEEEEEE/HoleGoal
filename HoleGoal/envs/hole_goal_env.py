1cd60250a880eebbb06bfbe24759ed16e3632aaf
Andre Le <andrelezx@gmail.com>
2023-02-19 18:11:42 -0700

    Copy over all code from non-gym code.

--- HoleGoal/envs/hole_goal_env.py
+++ HoleGoal/envs/hole_goal_env.py
@@ -2,23 +2,29 @@ import numpy as np
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
-
+from joblib import load, dump
+from termcolor import colored
 
 class HoleGoalEnv(gym.Env):
-  metadata = {'render.modes': ['human']}
+  metadata = {'render_modes': ['human']}
 
-  def __init__(self):
+  def __init__(self, render_mode=None):
     # 4 actions
     # 0 - Left, 1 - Down, 2 - Right, 3 - Up
-    self.col = 4
-    self.action_space = (4,)
+    self.action_space = spaces.Discrete(4)
 
     # 16 states, 4x4 grid
-    self.row = 16
-    self.observation_space = (4,4)
+    self.observation_space = spaces.Dict(
+      {
+        "agent": spaces.Box(0, 3, shape=(2,), dtype=int),
+        "target": spaces.Box(0, 3, shape=(2,), dtype=int),
+      }
+    )
 
     # setup the environment
-    self.q_table = np.zeros([self.row, self.col])
+    # Remember that the Q-table is supposed to be large
+    self.q_table = np.zeros([16, 4])
+    # The tables created and initialized here are now class attributes
     self.init_transition_table()
     self.init_reward_table()
 
@@ -32,6 +38,9 @@ class HoleGoalEnv(gym.Env):
     # in the long run, 10% exploration, 90% exploitation
     # meaning, the agent is never going to bore you with predictable moves :D
     self.epsilon_min = 0.1
+
+    assert render_mode is None or render_mode in self.metadata["render_modes"]
+    self.render_mode = render_mode
 
     # reset the environment
     self.reset()
@@ -69,7 +78,6 @@ class HoleGoalEnv(gym.Env):
     self.reward_table[13,3] = -100.
     self.reward_table[14,3] = 100.
     self.reward_table[14,2] = -100.
-
 
   def init_transition_table(self):
     """ TT[state_{i},action] = state_{i+1}
@@ -112,23 +120,6 @@ class HoleGoalEnv(gym.Env):
 
     self.transition_table = T
 
-  def step(self, action):
-    print('self.transition_table')
-    print(self.transition_table)
-    # determine the next_state given state and action
-    next_state = self.transition_table[self.state, action]
-
-    # done is True if next_state is Goal or Hole
-    done = next_state == 6 or next_state == 9 or next_state == 10 or next_state == 15
-
-    # reward given the state and action
-    reward = self.reward_table[self.state, action]
-
-    # the enviroment is now in new state
-    self.state = next_state
-
-    return next_state, reward, done
-
   # determine the next action
   def act(self):
     # 0 - Left, 1 - Down, 2 - Right, 3 - Up
@@ -150,11 +141,160 @@ class HoleGoalEnv(gym.Env):
 
     return np.argmax(self.q_table[self.state])
 
+  def step(self, action):
+    """Apply action to current state to get new state and reward."""
+    print('self.transition_table')
+    print(self.transition_table)
+    # determine the next_state given state and action
+    next_state = self.transition_table[self.state, action]
+
+    # done is True if next_state is Goal or Hole
+    done = next_state == 6 or next_state == 9 or next_state == 10 or next_state == 15
+
+    # reward given the state and action
+    reward = self.reward_table[self.state, action]
+
+    # the enviroment is now in new state
+    self.state = next_state
+
+    return next_state, reward, done
+  
+  # agent wins when the goal is reached
+  def is_in_win_state(self):
+    return self.state == 10
+
+  # Q-Learning - update the Q Table using Q(s, a)
+  def update_q_table(self, state, action, reward, next_state):
+    # Remember the Bellman equation to update the Q table?
+    # Q(s, a) = reward + gamma * max_a' Q(s', a')
+    q_value = reward + self.gamma * np.amax(self.q_table[next_state])
+
+    self.q_table[state, action] = q_value
+
+  # Print Q Table contents
+  def print_q_table(self):
+    print("Q-Table (Epsilon: %0.2f)" % self.epsilon)
+    print(self.q_table)
+
+  # update Exploration-Exploitation mix
+  def update_epsilon(self):
+    if self.epsilon > self.epsilon_min:
+        self.epsilon *= self.epsilon_decay
+
+  # Start of episode
   def reset(self):
     self.state = 0
     return self.state
     
+  # save member variables in a pickle
+  def save_q_world(self, joblib_file='joblibs/a_q_world_v1.joblib'):
+    dump([self.col, self.row, self.q_table, self.gamma, self.epsilon, 
+      self.epsilon_decay, self.epsilon_min, self.is_explore], joblib_file)
+    #f.close()
+
+  # load member variables from a pickle file
+  def load_q_world(self, joblib_file = 'joblibs/a_q_world_v1.joblib'):
+    [self.col, self.row, self.q_table, self.gamma, self.epsilon,
+      self.epsilon_decay, self.epsilon_min, self.is_explore] = load(joblib_file)
+  
   def render(self, mode='human', close=False):
-    ...
+    if self.render_mode == "rgb_array":
+      return self._render_frame()
+    
+  def state_coord(self, state):
+    return (state//4, 2+4*(state%4))
+    if state==0: return (0,2)
+    elif state==1: return (0,6)
+    elif state==2: return (0,10)
+    elif state==3: return (0,14)
+    
+  # UI to display agent moving on the grid
+  # Playing with the terminal, so you could see "kinda" animation!
+  def print_cell(self, row=0):
+    print("")
+    for i in range(17): #col
+      if self.state in [6,9,10,15]: #current state is one of the terminal states
+        #sys.exit('self.state in Hole or Goal state (state,i,row) = ({}, {}, {})'.format(self.state,i,row))
+        hole_cells = [(6,10,1),(9,6,2),(15,14,3)]
+        goal_cells = [(10,10,2)]
+        flag = False
+        color_goal = 'green'
+        if (self.state,i,row) in hole_cells:
+          marker = "\033[4mH\033[0m"
+          flag = True
+        elif (self.state,i,row) in goal_cells:
+          marker = "\033[4mG\033[0m"
+          flag = True
+        elif (i,row) in [(10,1),(6,2),(14,3)]:
+          marker = 'H' #hole state
+          flag= True
+        elif (i,row) in [(10,2)]:
+          marker = 'G' #goal state
+          flag = True
+
+        if i in [1,3,5,7,9,11,13,15] and flag==False: #none of the above gets printed
+          marker = ' ' #if (self.state, row,j) in cell else ' '
+        elif i in [2,6,10,14] and flag==False:
+          marker = ' '
+        elif flag==False:
+          marker = ''
+        
+        if (self.state,i,row) in goal_cells:#(self.state == 10 and row == 2 and i==10):
+          color_goal = 'red'
+        elif (self.state,i,row) in hole_cells:#(self.state == 6 and row == 1) or (self.state == 9 and row == 2) or (self.state == 15 and row == 3):
+          color_goal = 'blue'
+        else:
+          color_goal = 'green'
+        print(colored(marker, color_goal), end='')
+      else: #current state is a non-terminal state
+        #print('(i,row) = ({},{})'.format(i,row))
+        #if i in [2,6,10,14]:
+        #print('i = {}'.format(i))
+        color_goal = 'green'
+        H_G_flag = False
+        r,c = self.state_coord(self.state)
+      if (r,c) in [(row,i)]:
+        marker = '_'
+        color_goal = 'red'
+        H_G_flag = True
+      elif (i,row) in [(10,1),(6,2),(14,3)]:
+        marker = 'H' #hole state
+        H_G_flag = True
+      elif (i,row) in [(10,2)]:
+          marker = 'G' #goal state
+          H_G_flag = True
+
+      if i in [1,3,5,7,9,11,13,15] and H_G_flag==False: #none of the above gets printed
+        marker = ' ' #if (self.state, row,j) in cell else ' '
+      elif i in [2,6,10,14] and H_G_flag==False:
+        marker = ' '
+      elif H_G_flag==False:
+        marker = ''
+      print(colored(marker, color_goal), end='')
+
+      if i % 4 == 0: #every 4 col, close the cell with a boundary
+        print('|', end='')
+      #else:
+      #    print(' ', end='')
+    print("")
+    #sys.exit('Debug exit on row>={}'.format(row))
+
+  # UI to display mode and action of agent
+  def print_world(self, action, step):
+    actions = {0: "(Left)", 1: "(Down)", 2: "(Right)", 3: "(Up)"}
+    explore = "Explore" if self.is_explore else "Exploit"
+    print("Step", step, ":", explore, actions[action])
+    for _ in range(17):
+      print('-', end='')
+    self.print_cell() #row=0
+    for _ in range(17):
+      print('-', end='')
+    for r in [1,2,3]: #remaining rows
+      self.print_cell(row=r) #print row `r`
+      for _ in range(17):
+        print('-', end='')
+    print("")
+    #sys.exit("print_world")
+    
   def close(self):
     ...
--- my_gym_demo.py
+++ my_gym_demo.py
@@ -2,6 +2,108 @@
 import gym
 # The name of this module comes from setup.py
 import HoleGoal
+import numpy
+from tqdm import tqdm, trange
+import math
+import os
+from collections import deque
+import time
 
-# The name of this environment comes from HoleGoal/__init__.py
-env = gym.make('hole-goal-v0')
+# UI to display episode count
+def print_episode(episode, delay=1):
+    os.system('cls')
+    for _ in range(17):
+        print('=', end='')
+    print("")
+    print("Episode ", episode)
+    for _ in range(17):
+        print('=', end='')
+    print("")
+    time.sleep(delay)
+
+
+# UI to display the world, delay of 1 sec for ease of understanding
+def print_status(hg_env, action, done, step, delay=1,training_mode=True):
+    os.system('cls')
+    hg_env.print_world(action, step)
+    if training_mode: hg_env.print_q_table()
+    if done:
+        print("-------EPISODE DONE--------")
+        delay *= 2
+    time.sleep(delay)
+
+def main():
+    verbose = True
+    print('Do you want to train the agent? ',end=' ')
+    ans = input('[y/N] ').lower()
+    if ans in ['yes', 'y']:
+        training_flag = True
+    elif ans in ['no', 'n','N']:
+        training_flag = False
+    else:
+        print("Didn't understand your input! Guessing you do not want to train at all!")
+        training_flag = False
+
+    if training_flag == True:
+        maxwins = 100
+        delay = 0
+    else:
+        maxwins = 5
+        delay = 1
+
+    wins = 0
+    episode_count = 10 * maxwins
+    # scores (max number of steps bef goal) - good indicator of learning
+    scores = deque(maxlen=maxwins)
+
+    # The name of this environment comes from HoleGoal/__init__.py
+    hg_env = gym.make('hole-goal-v0', render_mode='human')
+    hg_env.__init__()
+
+    if training_flag == False:
+        #Load pre-existing Q-table
+        print("Loading q-world...")
+        hg_env.load_q_world()
+
+    step = 1
+    exit_flag = False
+    # state, action, reward, next state iteration
+    for episode in trange(episode_count):
+        # At the start of an episode, reset the agent's position
+        state = hg_env.reset()
+        done = False
+        print_episode(episode, delay=delay)
+        while not done:
+            print("Episode: ", episode + 1)
+            # Use the policy to get the next action
+            action = hg_env.act()
+            # Apply the action to the current state
+            next_state, reward, done = hg_env.step(action)
+            hg_env.update_q_table(state, action, reward, next_state)
+            print_status(hg_env, action, done, step, delay=delay,training_mode=training_flag)
+            # Update state
+            state = next_state
+            # Check if the episode terminated
+            if done:
+                if hg_env.is_in_win_state():
+                    wins += 1
+                    scores.append(step)
+                    if wins > maxwins:
+                        exit_flag = True
+                # Exploration-Exploitation is updated on episode end
+                hg_env.update_epsilon()
+                step = 1
+            else:
+                step += 1
+            if exit_flag==True:
+                break 
+        if exit_flag == True:
+            break
+    if training_flag == True:
+        print("Saving q-world for future use...")
+        hg_env.save_q_world()
+        print(scores)
+        hg_env.print_q_table()
+
+if __name__ == '__main__':
+    main()
